1bb37e1e25d7812d4a0196473797b53eded19560
Brandes <q7e-mha-u1@pool236pc05.edu.inform.hs-hannover.de>
2019-05-16 10:10:19 +0200

    [~](Updated) `7zr` changed to `7z`

--- appveyor/install_opengl.ps1
+++ appveyor/install_opengl.ps1
@@ -42,7 +42,8 @@ function DownloadOpenGL ($architecture) {
     if (Test-Path $filepathTmp) {
         Write-Host "File saved at" $filepathTmp
         # Unpack our zip-Archive
-        Invoke-Expression "& `".\tools\7zr.exe`" e -y ${filepathTmp}"
+        Invoke-Expression "& `"7z`" e ${filepathTmp}"
+        # Invoke-Expression "& `".\tools\7zr.exe`" e -y ${filepathTmp}"
         # [System.Diagnostics.Process]::Start(".\tools\7zr.exe", "e " + ($filepathTmp + "\" + $fileName))
     } else {
         # Retry once to get the error message if any at the last try
